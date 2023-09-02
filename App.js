ea9d9f21d583878597701365aa0e0705c1360214
Sairam Nulakani <sairamnulakani4@gmail.com>
2023-09-03 03:58:38 +0530

    login screen created

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+
+import StackNavigator from "./navigation/StackNavigator";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    regular: require("../personal_app/assets/fonts/Poppins-Regular.ttf"),
+    light: require("../personal_app/assets/fonts/Poppins-Light.ttf"),
+    bold: require("../personal_app/assets/fonts/Poppins-Bold.ttf"),
+    medium: require("../personal_app/assets/fonts/Poppins-Medium.ttf"),
+    extrabold: require("../personal_app/assets/fonts/Poppins-ExtraBold.ttf"),
+    semibold: require("../personal_app/assets/fonts/Poppins-SemiBold.ttf"),
+  });
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StackNavigator />
+    </>
   );
 }
 
