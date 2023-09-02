50a9d7cd5ee9ac881d265a15561510adf209a1d0
Sairam Nulakani <sairamnulakani4@gmail.com>
2023-09-03 04:15:35 +0530

    created register page with name,email,password fields

--- screens/Register.js
+++ screens/Register.js
@@ -1,11 +1,189 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  KeyboardAvoidingView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>Register</Text>
-    </View>
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: "white", alignItems: "center" }}
+    >
+      <View>
+        <Image
+          source={{
+            uri: "https://graphiccloud.net/wp-content/uploads/2021/07/Clean-Cart-Logo-Design.jpg",
+          }}
+          style={{ width: 150, height: 100 }}
+        />
+      </View>
+      <KeyboardAvoidingView>
+        <View style={{ alignItems: "center" }}>
+          <Text
+            style={{
+              fontFamily: "semibold",
+              fontSize: 17,
+              marginTop: 12,
+              color: "#041E42",
+            }}
+          >
+            Register to your Account
+          </Text>
+        </View>
+        <View style={{ marginTop: 70 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 5,
+              backgroundColor: "#D0D0D0",
+              paddingVertical: 7,
+              borderRadius: 5,
+              marginTop: 30,
+            }}
+          >
+            <Ionicons
+              style={{ marginLeft: 8 }}
+              name="person"
+              size={24}
+              color="gray"
+            />
+            <TextInput
+              style={{
+                color: "gray",
+                marginVertical: 10,
+                width: 300,
+                fontSize: 16,
+              }}
+              placeholder="Name"
+              value={name}
+              onChangeText={(text) => setName(text)}
+            />
+          </View>
+        </View>
+
+        <View style={{ marginTop: 5 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 5,
+              backgroundColor: "#D0D0D0",
+              paddingVertical: 7,
+              borderRadius: 5,
+              marginTop: 30,
+            }}
+          >
+            <MaterialCommunityIcons
+              style={{ marginLeft: 8 }}
+              name="email"
+              size={24}
+              color="gray"
+            />
+            <TextInput
+              style={{
+                color: "gray",
+                marginVertical: 10,
+                width: 300,
+                fontSize: 16,
+              }}
+              placeholder="Email"
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+            />
+          </View>
+        </View>
+        <View style={{ marginTop: 5 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 5,
+              backgroundColor: "#D0D0D0",
+              paddingVertical: 7,
+              borderRadius: 5,
+              marginTop: 30,
+            }}
+          >
+            <Entypo
+              style={{ marginLeft: 8 }}
+              name="lock"
+              size={24}
+              color="gray"
+            />
+            <TextInput
+              style={{
+                color: "gray",
+                marginVertical: 10,
+                width: 300,
+                fontSize: 16,
+              }}
+              placeholder="Password"
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+              secureTextEntry={true}
+            />
+          </View>
+        </View>
+        <View
+          style={{
+            marginTop: 12,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text>Keep me logged in</Text>
+          <Text style={{ color: "#007FFF", fontFamily: "semibold" }}>
+            Forgot Password
+          </Text>
+        </View>
+        <View style={{ marginTop: 50 }} />
+        <TouchableOpacity
+          style={{
+            width: 200,
+            backgroundColor: "#FEBE10",
+            borderRadius: 6,
+            marginLeft: "auto",
+            marginRight: "auto",
+            padding: 15,
+          }}
+        >
+          <Text
+            style={{
+              textAlign: "center",
+              color: "white",
+              fontSize: 16,
+              fontFamily: "bold",
+            }}
+          >
+            Register
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{ marginTop: 15 }}
+          onPress={() => navigation.goBack()}
+        >
+          <Text style={{ textAlign: "center", color: "gray", fontSize: 16 }}>
+            Already have an account ? login
+          </Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 };
 
