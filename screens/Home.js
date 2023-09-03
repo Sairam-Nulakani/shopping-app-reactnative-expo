f363d146aaba7ad275a6e8cb4be29221def9d57f
Sairam Nulakani <sairamnulakani4@gmail.com>
2023-09-04 01:28:45 +0530

    created products category slider with react native scrollview

--- screens/Home.js
+++ screens/Home.js
@@ -7,14 +7,61 @@ import {
   TextInput,
   TouchableOpacity,
   View,
+  Image,
 } from "react-native";
 import React from "react";
-import { Ionicons } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useNavigation } from "@react-navigation/native";
-import { FontAwesome } from "@expo/vector-icons";
+import {
+  FontAwesome,
+  MaterialIcons,
+  EvilIcons,
+  Ionicons,
+} from "@expo/vector-icons";
+import { SliderBox } from "react-native-image-slider-box";
 
 const Home = () => {
+  const list = [
+    {
+      id: "0",
+      image: "https://m.media-amazon.com/images/I/41EcYoIZhIL._AC_SY400_.jpg",
+      name: "Home",
+    },
+    {
+      id: "1",
+      image:
+        "https://m.media-amazon.com/images/G/31/img20/Events/Jup21dealsgrid/blockbuster.jpg",
+      name: "Deals",
+    },
+    {
+      id: "3",
+      image:
+        "https://images-eu.ssl-images-amazon.com/images/I/31dXEvtxidL._AC_SX368_.jpg",
+      name: "Electronics",
+    },
+    {
+      id: "4",
+      image:
+        "https://m.media-amazon.com/images/G/31/img20/Events/Jup21dealsgrid/All_Icons_Template_1_icons_01.jpg",
+      name: "Mobiles",
+    },
+    {
+      id: "5",
+      image:
+        "https://m.media-amazon.com/images/G/31/img20/Events/Jup21dealsgrid/music.jpg",
+      name: "Music",
+    },
+    {
+      id: "6",
+      image: "https://m.media-amazon.com/images/I/51dZ19miAbL._AC_SY350_.jpg",
+      name: "Fashion",
+    },
+  ];
+  const images = [
+    "https://img.etimg.com/thumb/msid-93051525,width-1070,height-580,imgsize-2243475,overlay-economictimes/photo.jpg",
+    "https://images-eu.ssl-images-amazon.com/images/G/31/img22/Wireless/devjyoti/PD23/Launches/Updated_ingress1242x550_3.gif",
+    "https://images-eu.ssl-images-amazon.com/images/G/31/img23/Books/BB/JULY/1242x550_Header-BB-Jul23.jpg",
+  ];
   const navigation = useNavigation();
   const handleLogout = () => {
     AsyncStorage.removeItem("authToken");
@@ -60,6 +107,51 @@ const Home = () => {
           </TouchableOpacity>
           <FontAwesome name="microphone" size={24} color="black" />
         </View>
+        <View
+          style={{
+            flexDirection: "row",
+            gap: 5,
+            alignItems: "center",
+            padding: 10,
+            backgroundColor: "#AFEEEE",
+          }}
+        >
+          <EvilIcons name="location" size={24} color="black" />
+          <TouchableOpacity>
+            <Text style={{ fontSize: 13, fontWeight: "500" }}>
+              Delivered to Sairam-Kondapur 500084
+            </Text>
+          </TouchableOpacity>
+          <MaterialIcons name="keyboard-arrow-down" size={24} color="black" />
+        </View>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          {list.map((item) => (
+            <TouchableOpacity
+              key={item.id}
+              style={{
+                margin: 10,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Image
+                source={{ uri: item.image }}
+                style={{ width: 50, height: 50, resizeMode: "contain" }}
+              />
+              <Text
+                style={{
+                  textAlign: "center",
+                  fontSize: 12,
+                  fontWeight: "500",
+                  marginTop: 5,
+                }}
+              >
+                {item.name}
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
+        <SliderBox />
       </ScrollView>
     </SafeAreaView>
   );
